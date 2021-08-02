f0418a0446c6d50441725a8ba63e369f3b8f035d
keishaputritheanny <pkeisha47@gmail.com>
2021-08-02 15:02:01 +0700

    first commit

---
+++ database/migrations/2021_07_28_020908_create_books_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('nisbn');
+            $table->string('title');
+            $table->longText('description');
+            $table->string('image_url');
+            $table->foreignId('author_id');
+            $table->foreignId('pulisher_id');
+            $table->double('rating')->default(0);
+            $table->integer('stock')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
